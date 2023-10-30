14b1f755fc54bab96e3b71bce45df1d73dd68d2d
rakeshmakvana <rakeshmakvana2108@gmail.com>
2023-10-30 13:00:09 +0530

    practice1

---
+++ Practice1/js/script.js
@@ -0,0 +1,438 @@
+// student1
+var st1 = {
+    id: 20231,
+    name: "Rakesh Makvana",
+    course: "Full Stack Devlopment",
+    bhirthdate: '21-08-2004',
+    zender: "Male",
+    mobile: 9558358450,
+    email: "rakeshmakvana2108@gmail.com",
+    addresh: "93,varsha-1,varacha road,surat-395006"
+}
+
+document.getElementById('id1').innerHTML += st1.id;
+document.getElementById('nm1').innerHTML += st1.name;
+document.getElementById('cr1').innerHTML += st1.course;
+document.getElementById('bd1').innerHTML += st1.bhirthdate;
+document.getElementById('zn1').innerHTML += st1.zender;
+document.getElementById('mb1').innerHTML += st1.mobile;
+document.getElementById('em1').innerHTML += st1.email;
+document.getElementById('ad1').innerHTML += st1.addresh;
+
+
+// student2
+var st2 = {
+    id: 20232,
+    name: "Ajay Patel",
+    course: "Frontend Devlopment",
+    bhirthdate: '03-02-2002',
+    zender: "Male",
+    mobile: 9935462389,
+    email: "ajayp@gmail.com",
+    addresh: "12-13,khodiyar nagar,ak road,surat-395003"
+}
+
+document.getElementById('id2').innerHTML += st2.id;
+document.getElementById('nm2').innerHTML += st2.name;
+document.getElementById('cr2').innerHTML += st2.course;
+document.getElementById('bd2').innerHTML += st2.bhirthdate;
+document.getElementById('zn2').innerHTML += st2.zender;
+document.getElementById('mb2').innerHTML += st2.mobile;
+document.getElementById('em2').innerHTML += st2.email;
+document.getElementById('ad2').innerHTML += st2.addresh;
+
+
+// student3
+var st3 = {
+    id: 20233,
+    name: "Raj Shah",
+    course: "App Devlopment",
+    bhirthdate: '10-12-2002',
+    zender: "Male",
+    mobile: 9734657234,
+    email: "raj@gmail.com",
+    addresh: "101,jadishnagar nagar,l-h road,surat-395005"
+}
+
+document.getElementById('id3').innerHTML += st3.id;
+document.getElementById('nm3').innerHTML += st3.name;
+document.getElementById('cr3').innerHTML += st3.course;
+document.getElementById('bd3').innerHTML += st3.bhirthdate;
+document.getElementById('zn3').innerHTML += st3.zender;
+document.getElementById('mb3').innerHTML += st3.mobile;
+document.getElementById('em3').innerHTML += st3.email;
+document.getElementById('ad3').innerHTML += st3.addresh;
+
+
+// student4
+var st4 = {
+    id: 20234,
+    name: "Priya Patel",
+    course: "UI/UX Designer",
+    bhirthdate: '16-02-2003',
+    zender: "Female",
+    mobile: 8634256799,
+    email: "priya16@gmail.com",
+    addresh: "flat no-202,varaj nagar,katargam,surat-396002"
+}
+
+document.getElementById('id4').innerHTML += st4.id;
+document.getElementById('nm4').innerHTML += st4.name;
+document.getElementById('cr4').innerHTML += st4.course;
+document.getElementById('bd4').innerHTML += st4.bhirthdate;
+document.getElementById('zn4').innerHTML += st4.zender;
+document.getElementById('mb4').innerHTML += st4.mobile;
+document.getElementById('em4').innerHTML += st4.email;
+document.getElementById('ad4').innerHTML += st4.addresh;
+
+
+// student5
+var st5 = {
+    id: 20235,
+    name: "Nandini Garg",
+    course: "UI/UX Designer",
+    bhirthdate: '01-03-2004',
+    zender: "Female",
+    mobile: 9352436475,
+    email: "nandinigarg@gmail.com",
+    addresh: "24-25,Gangeshwer Soc,varacha road,surat-395007"
+}
+
+document.getElementById('id5').innerHTML += st5.id;
+document.getElementById('nm5').innerHTML += st5.name;
+document.getElementById('cr5').innerHTML += st5.course;
+document.getElementById('bd5').innerHTML += st5.bhirthdate;
+document.getElementById('zn5').innerHTML += st5.zender;
+document.getElementById('mb5').innerHTML += st5.mobile;
+document.getElementById('em5').innerHTML += st5.email;
+document.getElementById('ad5').innerHTML += st5.addresh;
+
+
+// student6
+var st6 = {
+    id: 20236,
+    name: "Hemat Chohan",
+    course: "Graphic Designer",
+    bhirthdate: '18-04-2003',
+    zender: "Male",
+    mobile: 9394655453,
+    email: "hc123@gmail.com",
+    addresh: "first floor,rajhansh society,mani nagar,ahmdabad-434008"
+}
+
+document.getElementById('id6').innerHTML += st6.id;
+document.getElementById('nm6').innerHTML += st6.name;
+document.getElementById('cr6').innerHTML += st6.course;
+document.getElementById('bd6').innerHTML += st6.bhirthdate;
+document.getElementById('zn6').innerHTML += st6.zender;
+document.getElementById('mb6').innerHTML += st6.mobile;
+document.getElementById('em6').innerHTML += st6.email;
+document.getElementById('ad6').innerHTML += st6.addresh;
+
+
+// student7
+var st7 = {
+    id: 20237,
+    name: "Mira shah",
+    course: "Web Devlopment",
+    bhirthdate: '09-02-2002',
+    zender: "Female",
+    mobile: 9938645345,
+    email: "miruuu@gmail.com",
+    addresh: "19,vira circle,mem nagar,ahmdabad-445005"
+}
+
+document.getElementById('id7').innerHTML += st7.id;
+document.getElementById('nm7').innerHTML += st7.name;
+document.getElementById('cr7').innerHTML += st7.course;
+document.getElementById('bd7').innerHTML += st7.bhirthdate;
+document.getElementById('zn7').innerHTML += st7.zender;
+document.getElementById('mb7').innerHTML += st7.mobile;
+document.getElementById('em7').innerHTML += st7.email;
+document.getElementById('ad7').innerHTML += st7.addresh;
+
+
+// student8
+var st8 = {
+    id: 20238,
+    name: "Manoj patel",
+    course: "UI/UX Designer",
+    bhirthdate: '22-04-2003',
+    zender: "Male",
+    mobile: 92435664356,
+    email: "manoj00@gmail.com",
+    addresh: "22-23-24,pritam nagar,varacha road,surat-395004"
+}
+
+document.getElementById('id8').innerHTML += st8.id;
+document.getElementById('nm8').innerHTML += st8.name;
+document.getElementById('cr8').innerHTML += st8.course;
+document.getElementById('bd8').innerHTML += st8.bhirthdate;
+document.getElementById('zn8').innerHTML += st8.zender;
+document.getElementById('mb8').innerHTML += st8.mobile;
+document.getElementById('em8').innerHTML += st8.email;
+document.getElementById('ad8').innerHTML += st8.addresh;
+
+
+// student9
+var st9 = {
+    id: 20239,
+    name: "Rajesh Mani",
+    course: "Graphic Designer",
+    bhirthdate: '11-11-2003',
+    zender: "Male",
+    mobile: 9462636465,
+    email: "rajman@gmail.com",
+    addresh: "108,narayan Soc,ak road,surat-395007"
+}
+
+document.getElementById('id9').innerHTML += st9.id;
+document.getElementById('nm9').innerHTML += st9.name;
+document.getElementById('cr9').innerHTML += st9.course;
+document.getElementById('bd9').innerHTML += st9.bhirthdate;
+document.getElementById('zn9').innerHTML += st9.zender;
+document.getElementById('mb9').innerHTML += st9.mobile;
+document.getElementById('em9').innerHTML += st9.email;
+document.getElementById('ad9').innerHTML += st9.addresh;
+
+
+// student10
+var st10 = {
+    id: 202310,
+    name: "Yash Mishra",
+    course: "Game Devlopment",
+    bhirthdate: '22-04-2002',
+    zender: "Male",
+    mobile: 9394623436,
+    email: "yash02@gmail.com",
+    addresh: "8th floor,mira society,mota varacha,surat-392002"
+}
+
+document.getElementById('id10').innerHTML += st10.id;
+document.getElementById('nm10').innerHTML += st10.name;
+document.getElementById('cr10').innerHTML += st10.course;
+document.getElementById('bd10').innerHTML += st10.bhirthdate;
+document.getElementById('zn10').innerHTML += st10.zender;
+document.getElementById('mb10').innerHTML += st10.mobile;
+document.getElementById('em10').innerHTML += st10.email;
+document.getElementById('ad10').innerHTML += st10.addresh;
+
+
+// student11
+var st11 = {
+    id: 202311,
+    name: "Hansha Vinit",
+    course: "Web Devlopment",
+    bhirthdate: '10-12-2003',
+    zender: "Female",
+    mobile: 7354645345,
+    email: "hansha@gmail.com",
+    addresh: "8824,gajera circle,mira nagar,surat-398005"
+}
+
+document.getElementById('id11').innerHTML += st11.id;
+document.getElementById('nm11').innerHTML += st11.name;
+document.getElementById('cr11').innerHTML += st11.course;
+document.getElementById('bd11').innerHTML += st11.bhirthdate;
+document.getElementById('zn11').innerHTML += st11.zender;
+document.getElementById('mb11').innerHTML += st11.mobile;
+document.getElementById('em11').innerHTML += st11.email;
+document.getElementById('ad11').innerHTML += st11.addresh;
+
+
+// student12
+var st12 = {
+    id: 202312,
+    name: "Pinki Patel",
+    course: "Seibar Security",
+    bhirthdate: '09-02-2003',
+    zender: "Female",
+    mobile: 9224356766,
+    email: "pink22@gmail.com",
+    addresh: "222,mani nagar,ck road,ahmdabad-465004"
+}
+
+document.getElementById('id12').innerHTML += st12.id;
+document.getElementById('nm12').innerHTML += st12.name;
+document.getElementById('cr12').innerHTML += st12.course;
+document.getElementById('bd12').innerHTML += st12.bhirthdate;
+document.getElementById('zn12').innerHTML += st12.zender;
+document.getElementById('mb12').innerHTML += st12.mobile;
+document.getElementById('em12').innerHTML += st12.email;
+document.getElementById('ad12').innerHTML += st12.addresh;
+
+
+// student13
+var st13 = {
+    id: 202313,
+    name: "Mayur Mavani",
+    course: "Web Designer",
+    bhirthdate: '01-03-2002',
+    zender: "Male",
+    mobile: 7654345687,
+    email: "mavanimayur@gmail.com",
+    addresh: "89,vrundavan Soc,rk road,surat-355007"
+}
+
+document.getElementById('id13').innerHTML += st13.id;
+document.getElementById('nm13').innerHTML += st13.name;
+document.getElementById('cr13').innerHTML += st13.course;
+document.getElementById('bd13').innerHTML += st13.bhirthdate;
+document.getElementById('zn13').innerHTML += st13.zender;
+document.getElementById('mb13').innerHTML += st13.mobile;
+document.getElementById('em13').innerHTML += st13.email;
+document.getElementById('ad13').innerHTML += st13.addresh;
+
+
+// student14
+var st14 = {
+    id: 202314,
+    name: "Alpha Yagnik",
+    course: "Fluter Devlopment",
+    bhirthdate: '04-04-2004',
+    zender: "Female",
+    mobile: 3245723436,
+    email: "alpy@gmail.com",
+    addresh: "95,ashwin kumar,ak road,surat-395002"
+}
+
+document.getElementById('id14').innerHTML += st14.id;
+document.getElementById('nm14').innerHTML += st14.name;
+document.getElementById('cr14').innerHTML += st14.course;
+document.getElementById('bd14').innerHTML += st14.bhirthdate;
+document.getElementById('zn14').innerHTML += st14.zender;
+document.getElementById('mb14').innerHTML += st14.mobile;
+document.getElementById('em14').innerHTML += st14.email;
+document.getElementById('ad14').innerHTML += st14.addresh;
+
+
+// student15
+var st15 = {
+    id: 202315,
+    name: "Raj Vansh",
+    course: "Android Devlopment",
+    bhirthdate: '10-02-2003',
+    zender: "Male",
+    mobile: 2853445345,
+    email: "raj5243@gmail.com",
+    addresh: "11,vanita circle,city road,ahmdabad-498002"
+}
+
+document.getElementById('id15').innerHTML += st15.id;
+document.getElementById('nm15').innerHTML += st15.name;
+document.getElementById('cr15').innerHTML += st15.course;
+document.getElementById('bd15').innerHTML += st15.bhirthdate;
+document.getElementById('zn15').innerHTML += st15.zender;
+document.getElementById('mb15').innerHTML += st15.mobile;
+document.getElementById('em15').innerHTML += st15.email;
+document.getElementById('ad15').innerHTML += st15.addresh;
+
+
+// student16
+var st16 = {
+    id: 202316,
+    name: "Vishva Narayan",
+    course: "Machine Lerning",
+    bhirthdate: '08-01-2003',
+    zender: "Male",
+    mobile: 9224352266,
+    email: "vis12@gmail.com",
+    addresh: "51,satis park,ck road,ahmdabad-467004"
+}
+
+document.getElementById('id16').innerHTML += st16.id;
+document.getElementById('nm16').innerHTML += st16.name;
+document.getElementById('cr16').innerHTML += st16.course;
+document.getElementById('bd16').innerHTML += st16.bhirthdate;
+document.getElementById('zn16').innerHTML += st16.zender;
+document.getElementById('mb16').innerHTML += st16.mobile;
+document.getElementById('em16').innerHTML += st16.email;
+document.getElementById('ad16').innerHTML += st16.addresh;
+
+
+// student17
+var st17 = {
+    id: 202317,
+    name: "Vandita Mavani",
+    course: "App Designer",
+    bhirthdate: '30-03-2005',
+    zender: "Female",
+    mobile: 9923445687,
+    email: "vandiiii@gmail.com",
+    addresh: "second floor,cv Soc,katargam,surat-325002"
+}
+
+document.getElementById('id17').innerHTML += st17.id;
+document.getElementById('nm17').innerHTML += st17.name;
+document.getElementById('cr17').innerHTML += st17.course;
+document.getElementById('bd17').innerHTML += st17.bhirthdate;
+document.getElementById('zn17').innerHTML += st17.zender;
+document.getElementById('mb17').innerHTML += st17.mobile;
+document.getElementById('em17').innerHTML += st17.email;
+document.getElementById('ad17').innerHTML += st17.addresh;
+
+
+// student18
+var st18 = {
+    id: 202318,
+    name: "Lalji Mevada",
+    course: "Full Stack Devlopment",
+    bhirthdate: '28-10-2001',
+    zender: "Male",
+    mobile: 9987623436,
+    email: "lalo@gmail.com",
+    addresh: "34,mihir park,lp savani road,surat-392008"
+}
+
+document.getElementById('id18').innerHTML += st18.id;
+document.getElementById('nm18').innerHTML += st18.name;
+document.getElementById('cr18').innerHTML += st18.course;
+document.getElementById('bd18').innerHTML += st18.bhirthdate;
+document.getElementById('zn18').innerHTML += st18.zender;
+document.getElementById('mb18').innerHTML += st18.mobile;
+document.getElementById('em18').innerHTML += st18.email;
+document.getElementById('ad18').innerHTML += st18.addresh;
+
+
+// student19
+var st19 = {
+    id: 202319,
+    name: "Anita Kher",
+    course: "Web Devlopment",
+    bhirthdate: '11-12-2004',
+    zender: "Female",
+    mobile: 9853413234,
+    email: "akakak@gmail.com",
+    addresh: "555,mohan park,city road,ahmdabad-486002"
+}
+
+document.getElementById('id19').innerHTML += st19.id;
+document.getElementById('nm19').innerHTML += st19.name;
+document.getElementById('cr19').innerHTML += st19.course;
+document.getElementById('bd19').innerHTML += st19.bhirthdate;
+document.getElementById('zn19').innerHTML += st19.zender;
+document.getElementById('mb19').innerHTML += st19.mobile;
+document.getElementById('em19').innerHTML += st19.email;
+document.getElementById('ad19').innerHTML += st19.addresh;
+
+
+// student20
+var st20 = {
+    id: 202320,
+    name: "Kajal Patel",
+    course: "Softwer Enginier",
+    bhirthdate: '11-01-2001',
+    zender: "Female",
+    mobile: 9988243570,
+    email: "kp111@gmail.com",
+    addresh: "101,balaji park,kamrej,surat-367004"
+}
+
+document.getElementById('id20').innerHTML += st20.id;
+document.getElementById('nm20').innerHTML += st20.name;
+document.getElementById('cr20').innerHTML += st20.course;
+document.getElementById('bd20').innerHTML += st20.bhirthdate;
+document.getElementById('zn20').innerHTML += st20.zender;
+document.getElementById('mb20').innerHTML += st20.mobile;
+document.getElementById('em20').innerHTML += st20.email;
+document.getElementById('ad20').innerHTML += st20.addresh;
