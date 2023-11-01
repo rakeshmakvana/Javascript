b2539cba1b8552428b5777a67c915a28bb4a23e7
rakeshmakvana <rakeshmakvana2108@gmail.com>
2023-11-01 13:19:09 +0530

    practice2

---
+++ Practice2/js/script.js
@@ -0,0 +1,205 @@
+
+var write = [{
+    // 1
+    quotes: "The way I see it, if you want the rainbow, you gotta put up with the rain.",
+    author: "Dolly Parton"
+
+}, {
+    // 2
+    quotes: "Do all the good you can, for all the people you can, in all the ways you can, as long as you can.",
+    author: "Hillary Clinton"
+}, {
+    // 3
+    quotes: "Don’t settle for what life gives you; make life better and build something.",
+    author: "Ashton Kutcher"
+}, {
+    // 4
+    quotes: "Everything negative pressure, challenges is all an opportunity for me to rise.",
+    author: "Kobe Bryant"
+}, {
+    // 5
+    quotes: "You never really learn much from hearing yourself speak.",
+    author: "George Clooney"
+}, {
+    // 6
+    quotes: "Life imposes things on you that you can’t control, but you still have the choice of how you’re going to live through this.",
+    author: "Celine Dion"
+}, {
+    // 7
+    quotes: "Life is never easy. There is work to be done and obligations to be met – obligations to truth, to justice, and to liberty.",
+    author: "John F. Kennedy"
+}, {
+    // 8
+    quotes: "Live for each second without hesitation.",
+    author: "Elton John"
+}, {
+    // 9
+    quotes: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+    author: "Albert Einstein"
+}, {
+    // 10
+    quotes: "Life is really simple, but men insist on making it complicated.",
+    author: "Confucius"
+}, {
+    // 11
+    quotes: "Life is a succession of lessons which must be lived to be understood.",
+    author: "Helen Keller"
+}, {
+    // 12
+    quotes: "My mama always said, life is like a box of chocolates. You never know what you're gonna get.",
+    author: "Forrest Gump"
+}, {
+    // 13
+    quotes: "When we do the best we can, we never know what miracle is wrought in our life or the life of another.",
+    author: "Helen Keller"
+}, {
+    // 14
+    quotes: "The healthiest response to life is joy.",
+    author: "Deepak Chopra"
+}, {
+    // 15
+    quotes: "Life is like a coin. You can spend it any way you wish, but you only spend it once.",
+    author: "Lillian Dickson"
+}, {
+    // 16
+    quotes: "The best portion of a good man's life is his little nameless, unencumbered acts of kindness and of love.",
+    author: "Wordsworth"
+}, {
+    // 17
+    quotes: "In three words I can sum up everything I've learned about life: It goes on.",
+    author: "Robert Frost"
+}, {
+    // 18
+    quotes: "Life is ten percent what happens to you and ninety percent how you respond to it.",
+    author: "Charles Swindoll"
+}, {
+    // 19
+    quotes: "Keep calm and carry on.",
+    author: "Winston Churchill"
+}, {
+    // 20
+    quotes: "Maybe that’s what life is… a wink of the eye and winking stars.",
+    author: "Jack Kerouac"
+}, {
+    // 21
+    quotes: "Keep smiling, because life is a beautiful thing and there’s so much to smile about.",
+    author: "Marilyn Monroe"
+}, {
+    // 22
+    quotes: "Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship.",
+    author: "Buddha"
+}, {
+    // 23
+    quotes: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.",
+    author: "Dr. Seuss"
+}, {
+    // 24
+    quotes: "Good friends, good books, and a sleepy conscience: this is the ideal life.",
+    author: "Mark Twain"
+}, {
+    // 25
+    quotes: "The two most important days in your life are the day you are born and the day you find out why.",
+    author: "Mark Twain"
+}]
+
+
+// 1
+document.getElementById('qt1').innerHTML += write[0].quotes;
+document.getElementById('at1').innerHTML += write[0].author;
+
+// 2
+document.getElementById('qt2').innerHTML += write[1].quotes;
+document.getElementById('at2').innerHTML += write[1].author;
+
+// 3
+document.getElementById('qt3').innerHTML += write[2].quotes;
+document.getElementById('at3').innerHTML += write[2].author;
+
+// 4
+document.getElementById('qt4').innerHTML += write[3].quotes;
+document.getElementById('at4').innerHTML += write[3].author;
+
+// 5
+document.getElementById('qt5').innerHTML += write[4].quotes;
+document.getElementById('at5').innerHTML += write[4].author;
+
+// 6
+document.getElementById('qt6').innerHTML += write[5].quotes;
+document.getElementById('at6').innerHTML += write[5].author;
+
+// 7
+document.getElementById('qt7').innerHTML += write[6].quotes;
+document.getElementById('at7').innerHTML += write[6].author;
+
+// 8
+document.getElementById('qt8').innerHTML += write[7].quotes;
+document.getElementById('at8').innerHTML += write[7].author;
+
+// 9
+document.getElementById('qt9').innerHTML += write[8].quotes;
+document.getElementById('at9').innerHTML += write[8].author;
+
+// 10
+document.getElementById('qt10').innerHTML += write[9].quotes;
+document.getElementById('at10').innerHTML += write[9].author;
+
+// 11
+document.getElementById('qt11').innerHTML += write[10].quotes;
+document.getElementById('at11').innerHTML += write[10].author;
+
+// 12
+document.getElementById('qt12').innerHTML += write[11].quotes;
+document.getElementById('at12').innerHTML += write[11].author;
+
+// 13
+document.getElementById('qt13').innerHTML += write[12].quotes;
+document.getElementById('at13').innerHTML += write[12].author;
+
+// 14
+document.getElementById('qt14').innerHTML += write[13].quotes;
+document.getElementById('at14').innerHTML += write[13].author;
+
+// 15
+document.getElementById('qt15').innerHTML += write[14].quotes;
+document.getElementById('at15').innerHTML += write[14].author;
+
+// 16
+document.getElementById('qt16').innerHTML += write[15].quotes;
+document.getElementById('at16').innerHTML += write[15].author;
+
+// 17
+document.getElementById('qt17').innerHTML += write[16].quotes;
+document.getElementById('at17').innerHTML += write[16].author;
+
+// 18
+document.getElementById('qt18').innerHTML += write[17].quotes;
+document.getElementById('at18').innerHTML += write[17].author;
+
+// 19
+document.getElementById('qt19').innerHTML += write[18].quotes;
+document.getElementById('at19').innerHTML += write[18].author;
+
+// 20
+document.getElementById('qt20').innerHTML += write[19].quotes;
+document.getElementById('at20').innerHTML += write[19].author;
+
+// 21
+document.getElementById('qt21').innerHTML += write[20].quotes;
+document.getElementById('at21').innerHTML += write[20].author;
+
+// 22
+document.getElementById('qt22').innerHTML += write[21].quotes;
+document.getElementById('at22').innerHTML += write[21].author;
+
+// 23
+document.getElementById('qt23').innerHTML += write[22].quotes;
+document.getElementById('at23').innerHTML += write[22].author;
+
+// 24
+document.getElementById('qt24').innerHTML += write[23].quotes;
+document.getElementById('at24').innerHTML += write[23].author;
+
+// 25
+document.getElementById('qt25').innerHTML += write[24].quotes;
+document.getElementById('at25').innerHTML += write[24].author;
+
